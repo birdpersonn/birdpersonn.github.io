1a2b70d8d025bcfc45f12ea1aeadbde48720ff23
nellie <38364748+birdpersonn@users.noreply.github.com>
2023-08-19 08:23:49 -0700

    add projects to data

--- src/data/projects/index.tsx
+++ src/data/projects/index.tsx
@@ -1,12 +1,24 @@
 import {
+    SiCss3,
+    SiHtml5,
+    SiJavascript,
     SiReact,
     SiTailwindcss,
     SiTypescript,
     SiVite,
 } from 'react-icons/si';
 
-import PilatesDesktop from '@/assets/PilatesDesktop.webp';
+import PilatesDesktop from '@/assets/PilatesDesktop.png';
 import PilatesMobile from '@/assets/PilatesMobile.webp';
+
+import LibraryDesktop from '@/assets/LibraryDesktop.png';
+import LibraryMobile from '@/assets/LibraryMobile.png';
+
+import PortfolioDesktop from '@/assets/PortfolioDesktop.png';
+import PortfolioMobile from '@/assets/PortfolioMobile.png';
+
+import RPSDesktop from '@/assets/RockPaperScissorsDesktop.png';
+import RPSMobile from '@/assets/RockPaperScissorsMobile.png';
 
 const index = [
     {
@@ -22,6 +34,43 @@ const index = [
             <SiTailwindcss key={'Tailwind'} title={'Tailwind'} />,
         ]
     },
+    {
+        title: "Library",
+        demoLink: "https://birdpersonn.github.io/library/",
+        codeLink: "https://github.com/birdpersonn/library",
+        desktopImg: LibraryDesktop,
+        mobileImg: LibraryMobile,
+        icons: [
+            <SiReact key={'React'} title={'React'} />,
+            <SiJavascript key={'JavaScript'} title={'JavaScript'} />,
+            <SiTailwindcss key={'Tailwind'} title={'Tailwind'} />,
+        ]
+    },
+    {
+        title: "Portfolio",
+        demoLink: "https://birdpersonn.github.io/library/",
+        codeLink: "https://github.com/birdpersonn/library",
+        desktopImg: PortfolioDesktop,
+        mobileImg: PortfolioMobile,
+        icons: [
+            <SiVite key={'Vite'} title={'Vite'} />,
+            <SiReact key={'React'} title={'React'} />,
+            <SiTypescript key={'TypeScript'} title={'TypeScript'} />,
+            <SiTailwindcss key={'Tailwind'} title={'Tailwind'} />,
+        ]
+    },
+    {
+        title: "Rock Paper Scissors",
+        demoLink: "https://birdpersonn.github.io/rock-paper-scissors/",
+        codeLink: "https://github.com/birdpersonn/rock-paper-scissors/",
+        desktopImg: RPSDesktop,
+        mobileImg: RPSMobile,
+        icons: [
+            <SiJavascript key={'JavaScript'} title={'JavaScript'} />,
+            <SiHtml5 key={'HTML5'} title={'HTML5'} />,
+            <SiCss3 key={'CSS3'} title={'CSS3'} />
+        ]
+    }
 ];
 
 export default index;
--- src/scenes/projects/index.tsx
+++ src/scenes/projects/index.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 const Projects = ({ setSelectedPage }: Props) => {
     const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
-    const desktopStyles = "border-2 border-black shadow-opaque w-[85vw] md:w-4/5 object-cover";
+    const desktopStyles = "border-2 border-black shadow-opaque w-[85vw] md:w-4/5 md:h-full object-cover";
     const mobileStyles = "border-2 border-black shadow-opaque w-[20vw] md:w-[10vw] absolute left-[77vw] md:left-[69vw] object-cover";
     const { currentProject, pageNext, pagePrev } = useProjectsArr();
 
@@ -26,7 +26,7 @@ const Projects = ({ setSelectedPage }: Props) => {
                 {/* DESKTOP PREV PROJ ARROW */}
                 {isAboveMediumScreens && (
                     <button onClick={pagePrev}>
-                        <AiOutlineLeftSquare className="w-12 h-12 text-black hover:text-purple-dark" />
+                        <AiOutlineLeftSquare className="w-12 h-12 hover:text-black" />
                     </button>
                 )}
                 {/* PROJECT */}
@@ -64,18 +64,18 @@ const Projects = ({ setSelectedPage }: Props) => {
                 {/* DESKTOP NEXT PROJ ARROW */}
                 {isAboveMediumScreens && (
                     <button onClick={pageNext}>
-                        <AiOutlineRightSquare className="w-12 h-12 text-black hover:text-purple-dark" />
+                        <AiOutlineRightSquare className="w-12 h-12 hover:text-black" />
                     </button>
                 )}
 
                 {/* MOBILE PREV/NEXT PROJ ARROWS */}
                 {!isAboveMediumScreens && (
-                    <div className='flex gap-12 justify-center items-center pt-10 text-black'>
+                    <div className='flex gap-12 justify-center items-center pt-10'>
                         <button onClick={pagePrev}>
-                            <AiOutlineLeftSquare className="w-12 h-12" />
+                            <AiOutlineLeftSquare className="w-12 h-12 hover:text-black" />
                         </button>
                         <button onClick={pageNext}>
-                            <AiOutlineRightSquare className="w-12 h-12" />
+                            <AiOutlineRightSquare className="w-12 h-12 hover:text-black" />
                         </button>
                     </div>
                 )}
